922261feaa3803f64e1a1dc64b1d9829f9ad8046
Ari Porad <ari@ariporad.com>
2020-11-15 20:36:37 -0500

    refactor: code cleanup

--- recognize.m
+++ recognize.m
@@ -1,11 +1,11 @@
 %% Recognize an image using a pre-trained model
-% This function accepts a model and an image, then projects the image onto
-% the model's eigenvectors and finds its nearest neighbor, returning the
-% label and the match index.
-function [label, match, success] = recognize(model, img)
-    %% Image Preprocessing
-    % The nice thing about reshaping here is that it works even if the
-    % image has already been reshaped.
+% This function accepts a model and a series of points, then rasterizes the
+% image, projects the image onto the model's eigenvectors and finds its
+% nearest neighbor, returning if the match was successful, and (if it was)
+% the label and the match index.
+function [success, label, match] = recognize(model, points)
+    %% Rasterization
+    img = rasterize(points, 45);
     img = reshape(img, [size(img, 1) * size(img, 2), 1]);
     
     %% Image/Eigenvector Projection
@@ -14,7 +14,6 @@ function [label, match, success] = recognize(model, img)
     %% Matching and Labeling
     [match, distance] = knnsearch(model.training_imgs_projected', img_projected');
     
-    fprintf("KNNSearch Distance: %1.2f\n", distance);
     success = true; % TODO
     label = model.training_labels(match);
 end
--- run_draw.m
+++ run_draw.m
@@ -1,80 +1,69 @@
 %% Run the entire PCA process
-% This function is a wrapper over the entire recognition process (not
-% including training). It accepts a trained model, then prompts the user to
-% draw a symbol, rasterizes it, projects it onto the eigenvectors, and
-% finds the nearest neighbor.
-function [label, match] = run_draw(model)
+% This function is the entry point into our application. It prompts the
+% user to draw on a figure, then runs segmentation, runs PCA on each
+% segmented symbol, and outputs the result of segmentation back to the
+% figure.
+function [label, match_idx] = run_draw(model)
     processed_upto = 1;
-    last_stroke_end = 1;
+    last_stroke_end = -1;
     
-	%% Drawing
-    points = draw("Draw a Mathematical Character or Symbol:", @on_stroke_end);
-	
-%     %% Rasterization
-%     img = rasterize(points, 45);
-% 
-%     %% Sanity Check: Display Rasterized Image
-% 	figure; 
-%     imshow(img);
-%     title("You drew:");
-%     
-%     %% Character Recognition
-% 	[label, match] = recognize(model, img);
-% 
-%     %% Output
-%     title(sprintf("You drew a %s (#%1.0f):", label, match));
-% 	fprintf("You drew a %s (#%1.0f)!\n", label, match);
-    
-    function [success, label] = process_char(points)
-        %% Rasterization
-        img = rasterize(points, 45);
-
-        %% Sanity Check: Display Rasterized Image
-        %figure; 
-        %imshow(img);
-        %title("You drew:");
-
-        %% Character Recognition
-        [label, match, success] = recognize(model, img);
-
-        %% Output
-        %title(sprintf("You drew a %s (#%1.0f):", label, match));
-        fprintf("You drew a %s (#%1.0f)!\n", label, match);
-    end
+	%% Begin Drawing
+    start_draw("Draw Some Math:", @on_stroke_end);
     
     function on_stroke_end(points)
-        disp("here2");
-        if (last_stroke_end == 1)
-            last_stroke_end = size(points, 1)
+        %% Initial Condition: return if it's the first stroke
+        % Since we only process Stroke N when Stroke N+1 has been rendered
+        % and doesn't overlap, we never process on the first stroke.
+        if last_stroke_end == -1
+            last_stroke_end = size(points, 1);
             return
         end
         
-        pending_strokes = points(processed_upto:last_stroke_end, :);
-        new_stroke = points((last_stroke_end + 1):end, :);
+        %% Extract Points
+        % pending_points is all the points that haven't been processed by
+        % PCA yet, and new_points are all the points of the stroke that
+        % just ended (pending_points does not include new_points).
+        pending_points = points(processed_upto:last_stroke_end, :);
+        new_points = points((last_stroke_end + 1):end, :);
         
-        pending_mins = min(pending_strokes)
-        pending_maxes = max(pending_strokes)
-        new_mins = min(new_stroke)
-        new_maxes = max(new_stroke)
+        %% Detect Stroke Overlap
+        % We assume that a single character (for PCA purposes) is any
+        % number of strokes whose bounding boxes ovelap. Therefore, we
+        % can't process a stroke(s) until we know that the following stroke
+        % doesn't overlap.
+        pending_mins = min(pending_points);
+        pending_maxes = max(pending_points);
+        new_mins = min(new_points);
+        new_maxes = max(new_points);
         
-        
-        done_with_stroke = ...
+        done_with_symbol = ...
             (pending_maxes(1) < new_mins(1) || pending_mins(1) > new_maxes(1)) || ...
             (pending_maxes(2) < new_mins(2) || pending_mins(2) > new_maxes(2));
         
-        if done_with_stroke
-            [success, label] = process_char(pending_strokes);
-            
-            coords = min(pending_strokes)
-            dimensions = range(pending_strokes)
-            position = [coords dimensions]
-            
-            rectangle('Position', position, "EdgeColor", "red", "LineWidth", 2);
-            
-            text(coords(1) + (dimensions(1) / 2), coords(2) - 2, label, "FontSize", 12, "Color", "red", "HorizontalAlignment", "center");
-            processed_upto = last_stroke_end;
+        if ~done_with_symbol
+            last_stroke_end = size(points, 1);
+            return
         end
         
+        %% Character Recognition
+        [label, match_idx, success] = recognize(model, img);
+
+        %% Output
+        fprintf("You drew a %s (#%1.0f)!\n", label, match_idx);
+
+        %% Draw Results on Figure
+        coords = min(pending_points);
+        dimensions = range(pending_points);
+        position = [coords dimensions];
+
+        if success; color = "green"; else; color = "red"; end
+
+        rectangle('Position', position, "EdgeColor", color, "LineWidth", 2);
+        text(coords(1) + (dimensions(1) / 2), coords(2) - 2, label, ...
+            "FontSize", 12, "Color", color, "HorizontalAlignment", "center");
+        
+        %% Update State
+        processed_upto = last_stroke_end;
         last_stroke_end = size(points, 1);
     end
 end
